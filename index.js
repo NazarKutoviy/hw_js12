965bb87e417d4e651636a02010e2938f009a9574
NazarKutoviy <nazarkutoviy@gmail.com>
2024-11-04 19:05:09 +0200

    add

---
+++ index.js
@@ -0,0 +1,83 @@
+// 1
+// const user = {
+//   name: "mango",
+//   age: 20,
+//   hobby: "html",
+//   premium: true,
+// };
+// user.mood = "happy";
+// user.hobby = "skydiving";
+// user.premium = false;
+// const keys = Object.keys(user);
+// for (const key of keys) {
+//   console.log(`${key}:${user[key]}`);
+// }
+// 2
+// const countProps=function(obj){
+// return Object.keys(obj),length;
+// };
+// console.log(countProps({}));
+// console.log(countProps({name:"mango",age: 2}));
+// console.log(countProps({mail:"poly@mail.com",isOnline:true,score:500,}));
+// 3
+// const findBestLoyee = function (empLoyees) {
+//   let bestEmployee = "";
+//   let maxTasks = 0;
+//   for (empLoyee in employees) {
+//     if (empLoyees[employee] > maxTasks) {
+//       maxTasks = empLoyees[employee];
+//       bestEmployee = employee;
+//     }
+//   }
+//   return bestEmpLoyee;
+// };
+// console.log(
+//   findBestLoyee({
+//     ann: 29,
+//     david: 35,
+//     helen: 1,
+//     lorence: 99,
+//   })
+// );
+// console.log(
+//   findBestLoyee({
+//     poly: 12,
+//     mango: 17,
+//     ajax: 4,
+//   })
+// );
+// console.log(
+//   findBestLoyee({
+//     lux: 147,
+//     david: 21,
+//     kiwi: 19,
+//     chelsy: 38,
+//   })
+// );
+// 4
+// const countTotalSalary = function (empLoyees) {
+//   let totalSalary = 0;
+//   for (const salary of Object.values(empLoyees)) {
+//     totalSalary += salary;
+//   }
+//   return totalSalary;
+// };
+// console.log(countTotalSalary);
+// console.log(
+//   countTotalSalary({
+//     mango: 10,
+//     poly: 150,
+//     alfred: 80,
+//   })
+// );
+// console.log(
+//   countTotalSalary({
+//     kiwi: 200,
+//     lux: 50,
+//     chelsy: 150,
+//   })
+// );
+// 5
+const products=[
+name: 'radar',
+]
